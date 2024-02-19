3a087e789ad3f4ec434e66564ee7663fc1e0e685
mhilham <mhilham16@gmail.com>
2024-02-19 11:23:38 +0700

    menampilkan daftar produk

---
+++ main.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCksetmQe_ec2BH6g5MKqQU_1K1U6htmww",
+  authDomain: "data-7d32f.firebaseapp.com",
+  projectId: "data-7d32f",
+  storageBucket: "data-7d32f.appspot.com",
+  messagingSenderId: "156748846014",
+  appId: "1:156748846014:web:4269883b14bdb400b2dfef",
+  measurementId: "G-W3SBB85TF1"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambildaftarproduk() {
+  const refDokumen = collection(db, "produk");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikankueri = await getDocs(kueri);
+  
+  let hasil = [];
+  cuplikankueri.forEach((doc) => {
+    hasil.push({ 
+      id: dok.id, 
+      nama: dok.data().nama,
+      harga:dok.data().harga,
+      stok:dok.data().stok,
+      });
+  });
+  
+  return hasil;
+}
